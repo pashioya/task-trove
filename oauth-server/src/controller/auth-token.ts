d93c67800e06ce9c0118fdf425edb156fb0a4351
Frederik <60625517+freno10@users.noreply.github.com>
2024-04-19 18:20:05 +0200

    fix auth token controller

--- oauth-server/src/controller/auth-token.ts
+++ oauth-server/src/controller/auth-token.ts
@@ -6,65 +6,58 @@ import { ResponseStatus } from "@/enums/api";
 import { Request } from "express";
 import { env } from "@/env";
 
-const AuthTokenController = (req: Request, res: Response) => {
-    // TODO:Add more logging
+const AuthTokenController = async (req: Request, res: Response) => {
+  // TODO:Add more logging
 
-    // Extract authorization token from request
-    const authorizationToken = req.url.split(" ")[1];
+  // Extract authorization token from request
+  const authorizationToken = req.query.code;
 
-    // Check if authorization token exists
-    if (!authorizationToken) {
-        res.status(ResponseStatus.UNAUTHORIZED).json({
-            message: "Missing authorization token",
-        });
+  // Check if authorization token exists
+  if (!authorizationToken) {
+    return res.status(ResponseStatus.UNAUTHORIZED).json({
+      message: "Missing authorization token",
+    });
+  }
+
+  const clientId = env.MONDAY_CLIENT_ID;
+  const clientSecret = env.MONDAY_CLIENT_SECRET;
+
+  // get the main access token from env variables
+  const accessToken = env.ACCESS_TOKEN;
+
+  const storage = new Storage(accessToken);
+
+  try {
+    const response = await axios({
+      method: "post",
+      url: "https://auth.monday.com/oauth2/token",
+      data: {
+        client_id: clientId,
+        client_secret: clientSecret,
+        code: authorizationToken,
+        redirect_uri: "http://localhost:8080/auth-token",
+      },
+    });
+
+    const retrievedAccessToken = response.data.access_token;
+
+    const generated_token = jwt.sign({ retrievedAccessToken }, "secret", {
+      expiresIn: "1h",
+    });
+
+    await storage.set(generated_token, retrievedAccessToken);
+
+    if (!retrievedAccessToken) {
+      return res.status(ResponseStatus.INTERNAL_SERVER_ERROR).json({
+        message: "Failed to obtain access token",
+      });
     }
 
-    const clientId = env.MONDAY_CLIENT_ID;
-    const clientSecret = env.MONDAY_CLIENT_SECRET;
-
-    // get the main access token from env variables
-    const accessToken = env.ACCESS_TOKEN;
-
-    const storage = new Storage(accessToken);
-
-    try {
-        const response = axios({
-            method: "post",
-            url: "https://auth.monday.com/oauth2/token",
-            data: {
-                client_id: clientId,
-                client_secret: clientSecret,
-                code: authorizationToken,
-            },
-        });
-
-        const retrievedAccessToken = response.then(
-            (res) => res.data.access_token
-        );
-
-        const generated_token = jwt.sign({ retrievedAccessToken }, "secret", {
-            expiresIn: "1h",
-        });
-
-        storage.set(generated_token, retrievedAccessToken);
-
-        if (!retrievedAccessToken) {
-            res.status(ResponseStatus.INTERNAL_SERVER_ERROR).json({
-                message: "Failed to obtain access token",
-            });
-        }
-
-        res.status(ResponseStatus.OK).json({
-            message: "Access token obtained successfully",
-            temporaryCode: generated_token, // Add temporary code to response
-        }).redirect("exp://192.168.0.17:8081");
-
-    } catch (error) {
-        console.error("Error exchanging authorization token:", error);
-        res.status(ResponseStatus.INTERNAL_SERVER_ERROR).json({
-            message: "Failed to obtain access token",
-        });
-    }
+    return res.redirect("http://localhost:8081?token=" + generated_token);
+  } catch (error) {
+    console.error("Error exchanging authorization token:", error);
+    res.redirect("http://localhost:8081?error=" + "InvalidAuthorizationToken");
+  }
 };
 
 export default AuthTokenController;
