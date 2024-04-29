4d1f868861ab0512d801e3b2b47c3d40f4664211
pashioya <john.ashioya@gmail.com>
2024-04-29 15:42:21 +0200

    temporary file name changes

--- task-trove-app/app/(app)/(drawer)/(home)/index.tsx
+++ task-trove-app/app/(app)/(drawer)/(home)/index.tsx
@@ -1,8 +1,12 @@
-import Map from './Map'
+import * as Linking from 'expo-linking';
+import { Stack, Link } from 'expo-router';
+import { Text } from 'tamagui';
+
+import Map from './map-1';
+
+import { Container } from '~/tamagui.config';
 
 export default function Home() {
-    return <Map/>
-}
   const url = Linking.useURL();
 
   return (
@@ -10,8 +14,8 @@ export default function Home() {
       <Stack.Screen options={{ title: 'Home' }} />
       <Container>
         <Link href="/login">Login</Link>
-
         <Text>URL: {url}</Text>
+        <Map />
       </Container>
     </>
   );
