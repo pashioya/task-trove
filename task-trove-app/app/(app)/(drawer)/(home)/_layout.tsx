d60e8a97e39ce2211a7d27626228e755b8e39a4f
pashioya <john.ashioya@gmail.com>
2024-05-02 09:53:28 +0200

    renamed into kebab-case. removed map page

--- task-trove-app/app/(app)/(drawer)/(home)/_layout.tsx
+++ task-trove-app/app/(app)/(drawer)/(home)/_layout.tsx
@@ -4,7 +4,6 @@ export default function MapLayout() {
   return (
     <Stack initialRouteName="index">
       <Stack.Screen name="index" options={{ title: 'Home' }} />
-      <Stack.Screen name="map" options={{ title: 'Map' }} />
     </Stack>
   );
 }
