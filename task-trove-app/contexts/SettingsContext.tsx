eb08c011930a2b68d9c7018108652451aaaac6a4
noah.diderich <noah.diderich@student.kdg.be>
2024-05-08 10:44:23 +0200

    added pickers to chose which item from which board the user wants to update

--- task-trove-app/contexts/SettingsContext.tsx
+++ task-trove-app/contexts/SettingsContext.tsx
@@ -5,21 +5,21 @@ import type { Column } from '~/model/Column';
 import type { Item } from '~/model/Item';
 
 export type SettingsContext = {
-    board: Board;
-    column: Column;
-    item: Item;
-    setBoard: (board: Board) => void;
-    setColumn: (column: Column) => void;
-    setItem: (item: Item) => void;
-}
+  board: Board;
+  column: Column;
+  item: Item;
+  setBoard: (board: Board) => void;
+  setColumn: (column: Column) => void;
+  setItem: (item: Item) => void;
+};
 
 const SettingsContext = createContext<SettingsContext>({
-    board: {} as Board,
-    column: {} as Column,
-    item: {} as Item,
-    setBoard: () => {},
-    setColumn: () => {},
-    setItem: () => {},
+  board: {} as Board,
+  column: {} as Column,
+  item: {} as Item,
+  setBoard: () => {},
+  setColumn: () => {},
+  setItem: () => {},
 });
 
 export default SettingsContext;
--- task-trove-app/contexts/SettingsContextProvider.tsx
+++ task-trove-app/contexts/SettingsContextProvider.tsx
@@ -1,32 +1,32 @@
-import {useState, type ReactNode} from "react";
-import {Board} from "~/model/Board";
-import {Column} from "~/model/Column";
-import {Item} from "~/model/Item";
-import SettingsContext from "./SettingsContext";
+import { useState, type ReactNode } from 'react';
+import { Board } from '~/model/Board';
+import { Column } from '~/model/Column';
+import { Item } from '~/model/Item';
+import SettingsContext from './SettingsContext';
 
 type IWithChildren = {
-    children: ReactNode;
+  children: ReactNode;
 };
 
-const SettingsContextProvider = ({children}: IWithChildren) => {
-    const [board, setBoard] = useState<Board>({} as Board);
-    const [column, setColumn] = useState<Column>({} as Column);
-    const [item, setItem] = useState<Item>({} as Item);
+const SettingsContextProvider = ({ children }: IWithChildren) => {
+  const [board, setBoard] = useState<Board>({} as Board);
+  const [column, setColumn] = useState<Column>({} as Column);
+  const [item, setItem] = useState<Item>({} as Item);
 
-    return (
-        <SettingsContext.Provider
-            value={{
-                board,
-                column,
-                item,
-                setBoard,
-                setColumn,
-                setItem,
-            }}
-        >
-            {children}
-        </SettingsContext.Provider>
-    );
-}
+  return (
+    <SettingsContext.Provider
+      value={{
+        board,
+        column,
+        item,
+        setBoard,
+        setColumn,
+        setItem,
+      }}
+    >
+      {children}
+    </SettingsContext.Provider>
+  );
+};
 
 export default SettingsContextProvider;
