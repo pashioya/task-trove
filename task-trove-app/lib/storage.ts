ef265e14fdcfc55373ec5bc54f23eda82f9ac2c0
noah.diderich <noah.diderich@student.kdg.be>
2024-05-22 16:27:52 +0200

    change expo secure storage to react native async storage

--- task-trove-app/lib/storage.ts
+++ task-trove-app/lib/storage.ts
@@ -1,6 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export const expoSecureStorage = {
+export const asyncSecureStorage = {
   setItem: async (key: string, value: string) => await AsyncStorage.setItem(key, value),
   getItem: async (key: string) => (await AsyncStorage.getItem(key)) as Promise<string> | null,
   removeItem: async (key: string) => await AsyncStorage.removeItem(key),
