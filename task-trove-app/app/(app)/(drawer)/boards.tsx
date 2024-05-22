6062714d2080f3397dbfa98a691c7427875def06
pashioya <john.ashioya@gmail.com>
2024-05-22 23:24:59 +0200

    fixing versions

--- task-trove-app/app/(app)/(drawer)/boards.tsx
+++ task-trove-app/app/(app)/(drawer)/boards.tsx
@@ -1,6 +1,7 @@
 import { Link, Stack } from 'expo-router';
 
 import { Container } from '~/components/Container';
+import { ToastDemo } from '~/components/ToastDemo';
 
 export default function Boards() {
   return (
@@ -8,6 +9,7 @@ export default function Boards() {
       <Stack.Screen options={{ title: 'Boards' }} />
       <Container>
         <Link href="/">Home</Link>
+        <ToastDemo />
       </Container>
     </>
   );
--- task-trove-app/components/ToastDemo.tsx
+++ task-trove-app/components/ToastDemo.tsx
@@ -1,4 +1,10 @@
-import { Toast, useToastController, useToastState } from '@tamagui/toast';
+import {
+  Toast,
+  ToastProvider,
+  ToastViewport,
+  useToastController,
+  useToastState,
+} from '@tamagui/toast';
 
 import React from 'react';
 
@@ -14,7 +20,7 @@ import { Button, Label, Switch, XStack, YStack } from 'tamagui';
  */
 
 export const ToastDemo = () => {
-  const [native, setNative] = React.useState(false);
+  const [native, setNative] = React.useState(true);
   return (
     <YStack space alignItems="center">
       <ToastControl native={native} />
@@ -29,23 +35,26 @@ const CurrentToast = () => {
   if (!currentToast || currentToast.isHandledNatively) return null;
 
   return (
-    <Toast
-      key={currentToast.id}
-      duration={currentToast.duration}
-      enterStyle={{ opacity: 0, scale: 0.5, y: -25 }}
-      exitStyle={{ opacity: 0, scale: 1, y: -20 }}
-      y={0}
-      opacity={1}
-      scale={1}
-      animation="100ms"
-      viewportName={currentToast.viewportName}
-    >
-      <YStack>
-        <Toast.Title>{currentToast.title}</Toast.Title>
+    <ToastProvider>
+      <Toast
+        key={currentToast.id}
+        duration={currentToast.duration}
+        enterStyle={{ opacity: 0, scale: 0.5, y: -25 }}
+        exitStyle={{ opacity: 0, scale: 1, y: -20 }}
+        y={0}
+        opacity={1}
+        scale={1}
+        animation="100ms"
+        viewportName={currentToast.viewportName}
+      >
+        <YStack>
+          <Toast.Title>{currentToast.title}</Toast.Title>
 
-        {!!currentToast.message && <Toast.Description>{currentToast.message}</Toast.Description>}
-      </YStack>
-    </Toast>
+          {!!currentToast.message && <Toast.Description>{currentToast.message}</Toast.Description>}
+        </YStack>
+      </Toast>
+      <ToastViewport portalToRoot />
+    </ToastProvider>
   );
 };
 const ToastControl = ({ native }: { native: boolean }) => {
