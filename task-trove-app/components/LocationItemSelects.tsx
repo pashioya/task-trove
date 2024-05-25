704d03b562ef5824d026aebcfd9917a438ede584
noah.diderich <noah.diderich@student.kdg.be>
2024-05-25 15:04:03 +0200

    requested changes + minor changes

--- task-trove-app/components/LocationItemSelects.tsx
+++ task-trove-app/components/LocationItemSelects.tsx
@@ -93,9 +93,17 @@ export default function LocationItemSelects() {
         showAlert(columnsError);
       }
 
-      if (!columnsData || !columnsData.boards || !columnsData.boards[0] || !columnsData.boards[0].columns) return;
+      if (
+        !columnsData ||
+        !columnsData.boards ||
+        !columnsData.boards[0] ||
+        !columnsData.boards[0].columns
+      )
+        return;
 
-      const columns: Column[] = columnsData.boards[0].columns.filter((column): column is Column => column !== null);
+      const columns: Column[] = columnsData.boards[0].columns.filter(
+        (column): column is Column => column !== null,
+      );
       setColumns(columns);
 
       columnSelectItemsRef.current = columns.map(column => ({
@@ -116,7 +124,7 @@ export default function LocationItemSelects() {
       }
 
       if (!itemsData || !itemsData.boards || !itemsData.boards[0]) return;
-      const items: Item[] = itemsData.boards[0].items_page.items.filter((item): item is Item => item !== null);
+      const items = itemsData.boards[0].items_page.items as Item[];
       setItems(items);
 
       itemSelectItemsRef.current = items.map(item => ({
